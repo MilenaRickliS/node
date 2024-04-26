35faad5ea7eb95eebaf6c55e74f60196ae5ddba1
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-04-26 20:26:00 -0300

    c

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const requestListener = function (req, res) {
 if (req.url === "/") { // Se o URL da solicitação for "/", retorna "Você está na página inicial"
@@ -6,7 +7,14 @@ res.writeHead(200);
 res.end("Você está na página inicial");
 } else if (req.url === "/sobre") { // Se o URL da solicitação for "/sobre", retorna "Você está na página sobre"
 res.writeHead(200);
-res.end("Você está na página sobre");
+//res.end("Você está na página sobre");
+fs.readFile("sobre.txt", (err, data) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data);
+    res.end(data);
+  });
 } else { // Para qualquer outro URL, retorna "Página não encontrada"
 res.writeHead(404);
 res.end("Página não encontrada");
